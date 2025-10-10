42f61d18643b08e1d946fb3da43a4c61c0d0b96b
pachipico <pachipico1@gmail.com>
2025-10-10 19:26:11 +0900

    1010 FindTheIndexOfTheFirstOccurrenceInAString

--- findTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ findTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -2,10 +2,10 @@ package findTheIndexOfTheFirstOccurrenceInAString;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
 
-    // 0ms 걸린 첫번째 풀이
+    // 0ms 걸린 첫번째 풀이 - if문도 필요가 없었네
     public int strStr(String haystack, String needle) {
 
-        if(!haystack.contains(needle)) return -1;
+//        if(!haystack.contains(needle)) return -1;
         return haystack.indexOf(needle);
     }
 
